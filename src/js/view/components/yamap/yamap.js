4ae37360ca5af13c701a9cc98708c9a117fdce86
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-11-18 17:43:20 +0300

    add footer

--- src/js/view/components/yamap/yamap.js
+++ src/js/view/components/yamap/yamap.js
@@ -7,8 +7,21 @@ export async function initMap() {
         .then((maps) => {
             const map = new maps.Map("app", {
                 center: [59.963187, 30.302474],
-                zoom: 7,
+                zoom: 12,
             });
+            // Координаты для геометки
+            const coordinates = [59.963187, 30.302474];
+
+            // Создаем геометку
+            const placemark = new maps.Placemark(coordinates, {
+                // Описание при наведении на метку
+                hintContent: "Максимус",
+                // Содержимое метки
+                balloonContent: "Офис конного клуба Максимус",
+            });
+
+            // Добавляем геометку на карту
+            map.geoObjects.add(placemark);
         })
         .catch((error) => console.log("Failed to load Yandex Maps", error));
 }
